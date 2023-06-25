7cdb7c1e98fafe19f4be0e988395ba473cc3a476
mirzaarslan <mirzaarslan450@gmail.com>
2023-06-25 04:44:58 +0400

    Common module: di DispatchersModule.kt updated

--- core/common/src/main/java/com/core/common/di/DispatchersModule.kt
+++ core/common/src/main/java/com/core/common/di/DispatchersModule.kt
@@ -1,11 +1,18 @@
 package com.core.common.di
 
+import com.core.common.Dispatcher
+import com.core.common.SpDispatchers
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DispatchersModule {
-
+    @Provides
+    @Dispatcher(SpDispatchers.IO)
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
 }
