c80b699dca659a6b97833f560093d5ff7d6af59b
mirzaarslan <mirzaarslan450@gmail.com>
2023-06-28 08:20:56 +0400

    Trending module: actual implementation of Empty State added

--- core/common/src/main/java/com/core/common/result/Result.kt
+++ core/common/src/main/java/com/core/common/result/Result.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.onStart
 
 
 sealed interface Result<out T> {
-    data class Success<T>(val data: T) : Result<T>
+    data class Success<T>(val data: T?) : Result<T>
     data class Error(val exception: Throwable? = null) : Result<Nothing>
     object Loading : Result<Nothing>
 }
